e8d9dfe527f360d32cafe4c0f7fff1be18dd3d7b
Markus Mayer <widemeadows@gmail.com>
2019-09-04 00:22:20 +0200

    Add game UI

---
+++ Assets/Project/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private float _gameTime = 30;
+
+    [SerializeField]
+    private TextMeshProUGUI[] playerScoreText;
+
+    [SerializeField]
+    private TextMeshProUGUI timerText;
+
+    private int[] _playerScores;
+
+    public void AddPoints(int player, int amount)
+    {
+        _playerScores[player] += amount;
+        UpdateUI();
+    }
+
+    private void Start()
+    {
+        _playerScores = new int[2];
+        // TODO: Update timer text
+    }
+
+    private void Update()
+    {
+        _gameTime = Mathf.Max(0, _gameTime - Time.deltaTime);
+        timerText.text = $"{_gameTime:0.0}";
+
+        if (_gameTime <= 0)
+        {
+            // TODO: Game over!
+            Time.timeScale = 0;
+
+            // TODO: Note that there currently is no tie option.
+            var winnerIndex = _playerScores[0] > _playerScores[1] ? 0 : 1;
+        }
+    }
+
+    private void UpdateUI()
+    {
+        playerScoreText[0].text = $"{_playerScores[0]}";
+        playerScoreText[1].text = $"{_playerScores[1]}";
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Toads and Fireflies
+
+## Licenses
+
+This game is using the [Go Frog](https://fontsme.com/go-frog.font) font which is free to use for personal use.
